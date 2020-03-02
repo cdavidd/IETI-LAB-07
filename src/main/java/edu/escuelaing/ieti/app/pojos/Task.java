1ec52876262b3e1c360f85388a40912a1a7b8e2d
david <cdavidlopez@hotmail.com>
2020-03-02 11:39:11 -0500

    punto 2

---
+++ src/main/java/edu/escuelaing/ieti/app/pojos/Task.java
@@ -0,0 +1,32 @@
+package edu.escuelaing.ieti.app.pojos;
+
+public class Task {
+    private String description;
+    private User responsible;
+    private String status;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(User responsible) {
+        this.responsible = responsible;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
